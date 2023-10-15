1e0acbe70c5bce7fdf34f62c8660d7cf3f949228
shafna shah <shafnashah95@gmail.com>
2023-10-15 17:21:25 +0530

    modification

--- app.js
+++ app.js
@@ -112,4 +112,4 @@ app.delete('/expenses/:expense_id', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port 3000`));
--- genrate.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-// Function to generate a random secret key
-const generateSecretKey = () => {
-  return crypto.randomBytes(64).toString('hex');
-};
-
-// Output the generated secret key
-console.log(generateSecretKey());
